8c1faca9d113ee2af0488074fbf6110cafba76d0
lampochka112 <daniilgolosov232@gmail.com>
2025-03-02 18:17:02 +0300

    lol

---
+++ cycles.py
@@ -0,0 +1,10 @@
+#В Python существует два основных типа циклов: for и while.
+
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit) 
+    
+ number = 1
+while number <= 5:
+    print(number)
+    number += 1
