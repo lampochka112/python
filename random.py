69042d1a16351e5c21bf0aece2b055acb6ec7090
lampochka112 <daniilgolosov232@gmail.com>
2025-03-02 18:49:18 +0300

    lol

---
+++ random.py
@@ -0,0 +1,9 @@
+import random
+
+# Генерация случайного числа от 1 до 10
+print(random.randint(1, 10))
+
+# Перемешивание списка
+items = ['apple', 'banana', 'cherry']
+random.shuffle(items)
+print(items) 
