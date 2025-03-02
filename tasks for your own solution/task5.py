f5a60c6dbd3fae7ed472877c442b9ee9f8c3b2e2
lampochka112 <daniilgolosov232@gmail.com>
2025-03-02 20:24:23 +0300

    LOL

---
+++ tasks for your own solution/task5.py
@@ -0,0 +1,10 @@
+#  Факториал числа
+# Задание 5
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+number = int(input("Введите число для вычисления факториала: "))
+print(f"Факториал числа {number} равен: {factorial(number)}")
