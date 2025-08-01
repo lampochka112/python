139cd2c39ad56d1376366f8318b091f7914d0bc9
Jessie <daniilgolosov232@gmail.com>
2025-08-01 15:54:29 +0300

    Update Flask.py

--- Flask.py
+++ Flask.py
@@ -1,7 +1,7 @@
-# Простой веб-сервер с использованием Flask
+# Простой веб-сервер с использованием Flask.
  from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__).
 
 @app.route('/')
 def home():
@@ -9,5 +9,5 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True)
-if __name__ == 'main'
+
     
